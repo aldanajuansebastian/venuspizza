60a0095675f0fce03059551b4f8ba683aee739c5
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-12 10:22:52 -0500

    bg added

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './header2.css';
 import Astronauta from '../assets/astronauta.png';
+import Space from '../assets/spacebg.jpg'
 
 
 const Header2 = () => {
     return (
         <div className="header2">
-            
+            <img src={Space} alt="Fondo espacio" className="spacebg1"/>
             <div className="venuspizza2">
                 <h1>VenusPizza</h1>
             </div>

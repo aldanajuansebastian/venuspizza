71428f7ad94e5103ca0ae9b6f86bde09ee49b635
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-12 10:23:17 -0500

    bg added and css modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './header.css';
 import Astronauta from '../assets/astronauta.png';
 import {Link} from 'react-scroll';
+import Space from '../assets/spacebg.jpg'
 
 
 const Header = () => {
     return (
         <div className="header">
-            
+            <img src={Space} alt="Fondo espacio" className="spacebg"/>
             <div className="venuspizza">
                 <h1>VenusPizza</h1>
                 <h2>Una pizza de otro planeta</h2>
